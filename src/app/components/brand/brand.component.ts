13b70b41047266538017c36d06f0387941273496
ozgu kaptan <ozgukp@hotmail.com>
2021-03-13 01:53:49 +0300

    13-03-2021

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Brand } from 'src/app/models/brand';
+import {HttpClient} from '@angular/common/http'
+import { BrandResponseModel } from 'src/app/models/brandResponseModel';
+import { BrandService } from 'src/app/services/brand.service';
+
 
 @Component({
   selector: 'app-brand',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrandComponent implements OnInit {
 
-  constructor() { }
+  brands:Brand[] = [];
+  dataLoaded = false;
+  
+  constructor(private brandService:BrandService) { }
 
   ngOnInit(): void {
+    this.getBrands();
+  }
+
+  getBrands(){
+    this.brandService.getBrands()
+    .subscribe(Response=>{
+      this.brands = Response.data
+      this.dataLoaded= true});
   }
 
 }
